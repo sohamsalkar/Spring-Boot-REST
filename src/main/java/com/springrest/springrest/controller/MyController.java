0196ef2b6c10da49baeb4771c33009bb5fa7ce5f
Soham Salkar <salkarsoham2@gmail.com>
2021-04-06 18:02:23 +0530

    Update MyController.java

--- src/main/java/com/springrest/springrest/controller/MyController.java
+++ src/main/java/com/springrest/springrest/controller/MyController.java
@@ -2,9 +2,13 @@ package com.springrest.springrest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springrest.springrest.entities.Courses;
+
 import com.springrest.springrest.services.CourseService;
 import java.util.List;
 
@@ -28,7 +32,18 @@ public class MyController {
 		return this.courseService.getCourse();
 	}
 	
+	@GetMapping("/courses/{courseId}")
+	public Courses getSingleCourse(@PathVariable String courseId) 
+	{
+		return this.courseService.getCourse(Long.parseLong(courseId));
+	}
 	
+	@PostMapping("/courses")
+	public Courses addCourse(@RequestBody Courses course) 
+	{
+		return this.courseService.addCourse(course);
+	
+	}
 	
 
 }
